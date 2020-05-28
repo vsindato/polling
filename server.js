2221304bf221b3fbc27b21c5ea67f5bcc6024e47
Victor Sindato <victor@dhcp-18-20-203-20.dyn.mit.edu>
2020-05-28 14:56:43 -0400

    ...

--- server.js
+++ server.js
@@ -1,27 +1,35 @@
+// imports
 const app = require('express')();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
-const port = 5000;
-
-server.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-})
 
 
 // global variables
-
+const port = 5000;
 var roundCount = 0;
 var roundSubmissions = {};
 var allHeadlines = [];
 var submissionsPerRound = {0: allHeadlines};
-
-var aliveSockets = []; // ids of all connected sockets
-var aliveSubmitters = [];
-
-
+var aliveSockets = [];    // all connected sockets
+var aliveSubmitters = []; // all connected sockets with submissions
+var minRemainingHeadlines = 3; // to decide when to end poll
 
 
-// helper functions
+// routes
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+})
+
+
+// helper functions //
+function updateAliveSockets(id, operation) {
+  if (operation === 'insert'){
+    aliveSockets.push(id);
+  } else {
+    aliveSockets.splice(aliveSockets.indexOf(id), 1);
+  }
+  io.emit('participantUpdate', aliveSockets.length);  // broadcast new number of connected sockets
+}
 
 function updateRoundSubmissions(id, submission, operation){
   if (operation === 'insert'){
@@ -31,30 +39,27 @@ function updateRoundSubmissions(id, submission, operation){
   }
 }
 
-function updateAliveSockets(id, operation) {
-  if (operation === 'insert'){
-    aliveSockets.push(id);
-  } else {
-    aliveSockets.splice(aliveSockets.indexOf(id), 1); // remove socket's id from array of alive sessions
-  }
-  io.emit('participantUpdate', aliveSockets.length);
-}
-
 function updateAliveSubmitters(id, operation){
   if (operation === 'insert'){
     aliveSubmitters.push(id);
   } else {
     aliveSubmitters.splice(aliveSubmitters.indexOf(id), 1);
   }
-  io.emit('submissionsCount', `${aliveSubmitters.length}/${aliveSockets.length} users have submitted`);
+  io.emit('submissionsCount', `${aliveSubmitters.length}/${aliveSockets.length} submitted`); // broadcast new number of submitters
 }
 
+function contentsOf(array){
+  return array.toString();
+}
 
 function getArrayOfAllRoundSubmissions(submissions){
-  // input is an object where key= id, value = array
+  // submissions --> {id: array}
   var output = [];
   for (id in submissions) {
-    output = output.concat(submissions[id]);
+    // include unique submissions only
+    if (!output.includes(contentsOf(submissions[id]))){
+      output = output.concat(submissions[id]);
+    }
   }
   return output;
 }
@@ -64,34 +69,40 @@ function endRound(input) {
   submissionsPerRound[roundCount] = input;
   roundSubmissions = {};
   aliveSubmitters = [];
-
 }
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public/index.html');
-})
+function endPoll(){
+  roundCount = 0;
+}
+
 
 
 io.on('connection', (socket) => {
-  // Add new sessions to ones that have been observed
+
   if (!aliveSockets.includes(socket.id)) {
     updateAliveSockets(socket.id, 'insert');
   }
 
-  // submission event
   socket.on('submission', function(submission){
-    // submission is an array
-    // prevent single user from increasing submission count more than once
-    if (!aliveSubmitters.includes(socket.id)){
+    // submission --> array
+
+    if (aliveSubmitters.includes(socket.id)){
+      updateRoundSubmissions(socket.id, submission, 'insert');
+    } else {
       updateRoundSubmissions(socket.id, submission, 'insert');
       updateAliveSubmitters(socket.id, 'insert');
     }
 
-    // when everyone has submitted
     if (aliveSubmitters.length === aliveSockets.length){
       var roundSubmissionsArray = getArrayOfAllRoundSubmissions(roundSubmissions); // so that clients don't have to each do this separately
       io.emit('allSubmissions', {'roundSubmissionsArray': roundSubmissionsArray, 'roundSubmissions': roundSubmissions});
       endRound(roundSubmissionsArray);
+
+      // when poll is over
+      if (roundSubmissionsArray.length <= minRemainingHeadlines){
+        endPoll();
+        io.emit('pollClosing', roundSubmissionsArray);
+      }
     }
 
   })
@@ -104,4 +115,9 @@ io.on('connection', (socket) => {
     }
     updateRoundSubmissions(socket.id, null, 'remove');
   })
+})
+
+
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 })
